0874df67f59985531e17803637d4ee69f45bd7dd
yuanpeng <984337740@qq.com>
2019-09-12 10:23:56 +0800

    no message

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import * as THREE  from './build/three.module.js';
+import { CarControls } from './threeMaster/jsm/misc/CarControls';
+import { DRACOLoader } from './threeMaster/jsm/loaders/DRACOLoader';
+import { GLTFLoader } from './threeMaster/jsm/loaders/GLTFLoader';
+import { PMREMCubeUVPacker } from './threeMaster/jsm/pmrem/PMREMCubeUVPacker';
+import { PMREMGenerator } from './threeMaster/jsm/pmrem/PMREMGenerator';
+
+export { THREE , CarControls , DRACOLoader , GLTFLoader , PMREMCubeUVPacker , PMREMGenerator  };
